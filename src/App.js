2ec329e8babdf8118cc66be1e2345fe14085db0a
Momir <momirilic97@gmail.com>
2023-02-02 17:44:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Phonebook() {
+
+const storedEntries = JSON.parse(sessionStorage.getItem('entries')) || JSON.parse(localStorage.getItem('entries')) || {};
+const [entries, setEntries] = useState(storedEntries);
+
+const handleSubmit = (e) => {
+e.preventDefault();
+const name = e.target.name.value;
+const phone = e.target.phone.value;
+setEntries({ ...entries, [name]: phone });
+
+sessionStorage.setItem('entries', JSON.stringify({ ...entries, [name]: phone }));
+localStorage.setItem('entries', JSON.stringify({ ...entries, [name]: phone }));
 }
 
-export default App;
+const handleDelete = (name) => {
+const newEntries = { ...entries };
+delete newEntries[name];
+setEntries(newEntries);
+
+sessionStorage.setItem('entries', JSON.stringify(newEntries));
+localStorage.setItem('entries', JSON.stringify(newEntries));
+}
+
+return (
+
+<div>
+
+<h1>Phonebook App</h1>
+
+<form onSubmit={handleSubmit}>
+<label>
+<span id='name'>First and last name : </span>
+<input type="text" name="name" />
+</label>
+
+<label>
+
+  <br></br>
+
+  <span id='number'>Phone Number : </span>
+<input type="text" name="phone" />
+</label>
+
+<br></br>
+
+<button id='addbtn' type="submit">Add Contact</button>
+
+</form>
+
+<h2>Contacts :</h2>
+
+<ul>
+{Object.keys(entries).map((name) => (
+<li key={name}>
+{name}: {entries[name]}
+<button id='dltbtn' onClick={() => handleDelete(name)}>X</button>
+</li>
+))}
+</ul>
+</div>
+);
+}
+
+export default Phonebook;
